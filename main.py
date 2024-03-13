451a48e8f0d7e0d3f001c67456808289a8c69a76
UdenilsonEspiritoSanto <udenilson1017@yahoo.com.br>
2024-03-12 21:23:10 -0300

    inserir no banco

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ class Todo:
         self.page.window_resizable = False
         self.page.window_always_on_top = True
         self.page.title = 'TodoApp'
+        self.db_execute('CREATE TABLE IF NOT EXISTS tasks(name,status)')
+        self.task=""
         self.main_page()
     
     def db_execute(self,query,params=[]):
@@ -20,21 +22,33 @@ class Todo:
               
          
     def tasks_container(self):
+
         return ft.Container(
-            
+            height=self.page.height *0.8,
             content=ft.Column(
                 controls=[
                     ft.Checkbox(label='Tarefa-1',value=True)
                 ]
             )
         )
+    def set_value(self,e):
+         self.task = e.control.value
+    #funcao inserir dados
+    def add(self,e,input_task):
+        name = self.task
+        status = "incompleto"
+        if name:
+             self.db_execute(query = 'INSERT INTO tasks VALUES(?,?)',params=[name,status])
+             input_task.value = ""
     def main_page(self):
-           input_task = ft.TextField(hint_text="Dgite a Tarefa",expand=True)
+           input_task = ft.TextField(hint_text="Dgite a Tarefa",expand=True,
+                                     on_change=self.set_value)
 
            input_bar = ft.Row(
                 controls=[
                      input_task,
-                     ft.FloatingActionButton(icon=ft.icons.ADD)
+                     ft.FloatingActionButton(icon=ft.icons.ADD,
+                                             on_click=lambda e:self.add(e,input_task))
                 ]
            )
            tab = ft.Tabs(
